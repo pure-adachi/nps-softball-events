29902562999f751ceb2e3405a1a0570fb6aca7b5
pure-adachi <takuya_adachi@pure-system.com>
2025-08-24 18:34:53 +0900

    github pagesとreact-routerの404対応

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import { Navigate, createBrowserRouter, RouterProvider } from "react-router";
 import Home from "./components/pages/Home";
 import Event from "./components/pages/Event";
 
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "/events/:year",
+      element: <Event />
+    },
+    {
+      path: "*",
+      element: <Navigate to="/" replace />
+    }
+  ],
+  {
+    basename: "nps-softball-events"
+  }
+);
+
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/events/:year" element={<Event />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
